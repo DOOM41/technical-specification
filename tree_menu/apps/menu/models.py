0e816f9e7f098d18508d08397eba8310a4b581ee
Duman <98916649+DOOM41@users.noreply.github.com>
2023-01-25 15:02:18 +0600

    fix some bugs

--- tree_menu/apps/menu/models.py
+++ tree_menu/apps/menu/models.py
@@ -12,17 +12,22 @@ class MenuManager(Manager):
     def get_menu_and_submenu_items(self, name: str) -> dict:
         # Check name for some data
         if not name:
-            sub_menu: QuerySet[SubMenu] = SubMenu.objects.all()
+            sub_menu: QuerySet[SubMenu] = SubMenu.objects.select_related(
+                'menu').all()
+
         else:
-            sub_menu: QuerySet[SubMenu] = SubMenu.objects.filter(
-                menu__name=name
-            )
-        # Make result dict 
+            sub_menu: QuerySet[SubMenu] = SubMenu.objects.select_related(
+                'menu').filter(name=name)
+
+        # Make result dict
         result = {}
         for menu_item in sub_menu:
-            result[menu_item.menu.name] = sub_menu.filter(
-                menu__name=menu_item.menu.name
-            )
+            result[menu_item.menu.name] = []
+        
+        for menu_item in sub_menu:
+            for i in result.keys():
+                if i == menu_item.menu.name:
+                    result[menu_item.menu.name].append(menu_item)
         # Sort data 
         sorted_dict = dict(sorted(result.items()))
         return sorted_dict
